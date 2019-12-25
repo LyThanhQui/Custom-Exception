d642972d2b488cd72d5cac9e8b9b11db714b3963
Qui <lquiitfuture@gmail.com>
2019-12-25 23:32:43 +0700

    Update program.cs file

--- Program.cs
+++ Program.cs
@@ -1,43 +1,79 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Custom_Exception
 {
-    public class OddNumberException : Exception
+    //example1
+    /* public class OddNumberException : Exception
+     {
+         //Overriding the Message property
+         public override string Message
+         {
+             get
+             {
+                 return "divisor cannot be odd number";
+             }
+         }
+     }
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             int x, y, z;
+             Console.WriteLine("ENTER TWO INTEGER NUMBERS:");
+             x = int.Parse(Console.ReadLine());
+             y = int.Parse(Console.ReadLine());
+             try
+             {
+                 if (y % 2 > 0)
+                 {
+                    // OddNumberException ONE = new OddNumberException();
+                     //throw ONE;
+                    throw new OddNumberException();
+                 }
+                 z = x / y;
+                 Console.WriteLine(z);
+             }
+             catch (OddNumberException one)
+             {
+                 Console.WriteLine(one.Message);
+             }
+             Console.WriteLine("End of the program");
+             Console.ReadKey();
+         }
+     }*/
+    //Example2: Create a custom exception in C#:
+    [Serializable]
+    public class UserAlreadyLoggedInException : Exception
     {
-        //Overriding the Message property
-        public override string Message
+        public UserAlreadyLoggedInException(string message)
+            : base(message)
         {
-            get
-            {
-                return "divisor cannot be odd number";
-            }
+        }
+        public UserAlreadyLoggedInException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+        public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            int x, y, z;
-            Console.WriteLine("ENTER TWO INTEGER NUMBERS:");
-            x = int.Parse(Console.ReadLine());
-            y = int.Parse(Console.ReadLine());
             try
             {
-                if (y % 2 > 0)
-                {
-                    //OddNumberException ONE = new OddNumberException();
-                    //throw ONE;
-                    throw new OddNumberException();
-                }
-                z = x / y;
-                Console.WriteLine(z);
+                throw new UserAlreadyLoggedInException("User Already logged in");
             }
-            catch (OddNumberException one)
+            catch (UserAlreadyLoggedInException ex)
             {
-                Console.WriteLine(one.Message);
+                Console.WriteLine(ex.Message);
             }
             Console.WriteLine("End of the program");
             Console.ReadKey();
         }
     }
+
 }
